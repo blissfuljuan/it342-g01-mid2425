7a62474c65d5d9f779812d20cfe386bb3c5b4240
LAB12_USER ON WS23 <L12X15W23@citulab.net>
2025-06-23 17:58:00 +0800

    updated google contacts

---
+++ delmar-kiana-journal-activity/googlecontacts/src/main/java/com/delmar/googlecontacts/GoogleContactsApplication.java
@@ -0,0 +1,13 @@
+package com.delmar.googlecontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleContactsApplication.class, args);
+	}
+
+}
