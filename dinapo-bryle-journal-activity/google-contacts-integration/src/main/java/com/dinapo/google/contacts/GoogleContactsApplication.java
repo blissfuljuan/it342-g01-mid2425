1f914863e7b2dc07f694f51022780bdc5c1bf630
LAB12_USER ON WS40 <L12X15W40@citulab.net>
2025-06-25 16:13:02 +0800

    Google Contacts

---
+++ dinapo-bryle-journal-activity/google-contacts-integration/src/main/java/com/dinapo/google/contacts/GoogleContactsApplication.java
@@ -0,0 +1,13 @@
+package com.dinapo.google.contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleContactsApplication.class, args);
+	}
+
+}
